49b4e46676235c32ed58e39078530502c2d424be
Marianna <mckd2018@gmail.com>
2018-09-04 16:59:07 -0700

    add Mad Libs, add values and value types

--- madLibs.py
+++ madLibs.py
@@ -1,1 +1,10 @@
 # Madlib Story:
+
+# Parts of Speech Mad Libs from Everyday Speech
+Tina and John walked deep into the __dark(adjective)___ woods.
+They came upon a __creaky(adjective)__ old house.
+The house looked so __run-down(adjective)__ and creepy.
+On the front porch sat an old large __cat (noun)__.
+Tina thought she heard a __high-pitched (adjective)__ sound coming from the
+house.
+John __jumped(past tense verb) so high he touched a tree branch!
